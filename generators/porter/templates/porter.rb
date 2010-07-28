3ac44ccf66b3c8952a4a85ec75e8897903d0b1c0
Robert Bousquet <robert@robertbousquet.com>
2010-07-28 00:39:25 -0700

    Fixed errors in production where the porter gem isnt installed and isnt really required in the Rakefile

--- generators/porter/templates/porter.rb
+++ generators/porter/templates/porter.rb
@@ -1,2 +1,4 @@
 $VERBOSE = nil
-Dir["#{Gem.searcher.find('porter').full_gem_path}/lib/tasks/*.rake"].each { |ext| load ext }
+if porter = Gem.searcher.find('porter')
+  Dir["#{porter.full_gem_path}/lib/tasks/*.rake"].each { |ext| load ext }
+end
