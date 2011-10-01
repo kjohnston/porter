10074e29e7c974157acfa93617d842627ff41844
Kenny Johnston <kjohnston.ca@gmail.com>
2011-10-01 01:54:14 -0700

    Remove app and domain instance variables from generator.

--- lib/generators/porter/porter_generator.rb
+++ lib/generators/porter/porter_generator.rb
@@ -1,19 +1,14 @@
-require 'rails/generators'
+require "rails/generators"
 
 class PorterGenerator < Rails::Generators::Base
 
-  attr_accessor :app, :domain
-
   def self.source_root
-    @source_root ||= File.join(File.dirname(__FILE__), 'templates')
+    @source_root ||= File.join(File.dirname(__FILE__), "templates")
   end
 
   def create_config_file
-    @app    = Rails.root.to_s.split('/').last
-    @domain = @app + (@app.include?('.') ? '' : '.com')
-
-    template 'porter_config.yml', 'config/porter_config.yml'
-    readme 'INSTALL'
+    template "porter_config.yml", "config/porter_config.yml"
+    readme "INSTALL"
   end
 
 end
