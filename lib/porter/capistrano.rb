1726cb87f2b145bba85fb98c4c340b2474fac924
Kenny Johnston <kjohnston.ca@gmail.com>
2012-03-07 13:47:45 -0800

    Implement support for optional tables to ignore during mysqldump with configuration via config file or environment variable.

--- lib/porter/capistrano.rb
+++ lib/porter/capistrano.rb
@@ -14,14 +14,26 @@ if instance = Capistrano::Configuration.instance
         run "cat #{deploy_to}/current/config/database.yml" do |channel, stream, data|
           db_yml << data
         end
-        db_config   = YAML::load(db_yml)[stage.to_s]
-        db_name     = db_config["database"]
-        db_username = db_config["username"]
-        db_password = db_config["password"]
+        db_config      = YAML::load(db_yml)[stage.to_s]
+        db_name        = db_config["database"]
+        db_username    = db_config["username"]
+        db_password    = db_config["password"]
+        db_credentials = "--user=#{db_username} --password=#{db_password} "
 
+        porter_config = YAML::load_file("config/porter_config.yml")
+
+        if ENV["IGNORE_TABLES"]
+          tables_to_ignore = ENV["IGNORE_TABLES"].split(" ")
+        elsif stage_config = porter_config[stage.to_s] and stage_config["ignore_tables"]
+          tables_to_ignore = stage_config["ignore_tables"].split(" ")
+        end
+        ignore_tables = (tables_to_ignore || []).map { |table_name| "--ignore-table=#{db_name}.#{table_name.strip}" }.join(" ")
+
+        # Run mysqldump on the stage
         puts "Creating compressed backup of #{db_name} database on #{domain}..."
-        run "mysqldump --user=#{db_username} --password=#{db_password} #{db_name} | gzip > ~/#{db_name}.sql.gz"
+        run "mysqldump #{db_credentials} #{db_name} #{ignore_tables} | gzip > ~/#{db_name}.sql.gz"
 
+        # Issue rake task to restore the database backup
         system "rake porter:db DOMAIN=#{domain} DATABASE=#{db_name}"
       end
 
